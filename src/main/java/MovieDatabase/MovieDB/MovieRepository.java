400f7aa6c243c7d522f62817b58f7cc0969242d5
Shales <n0236074@lm.lmig.com>
2017-06-28 14:28:28 -0400

    we can connect to database

---
+++ src/main/java/MovieDatabase/MovieDB/MovieRepository.java
@@ -0,0 +1,11 @@
+package MovieDatabase.MovieDB;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+@Query("select m from Movie m where movieName = ?1 and director = ?2")
+List<Movie> findBYmovieName(String movieName, String director);
+}
