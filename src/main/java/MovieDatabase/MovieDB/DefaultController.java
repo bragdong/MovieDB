4fadb8c5a1b352cf76b8ea4b15bd0a11d9aaf007
Shales <n0236074@lm.lmig.com>
2017-07-08 10:34:49 -0400

    Took security out of old-state branch

--- src/main/java/MovieDatabase/MovieDB/DefaultController.java
+++ src/main/java/MovieDatabase/MovieDB/DefaultController.java
@@ -1,37 +1,37 @@
-package MovieDatabase.MovieDB;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class DefaultController {
-	
-	
-	    @RequestMapping(value="/")
-	    public String jspIndex() {
-	        return "index";
-	    }
-
-	    @RequestMapping(value="/login")
-	    public String login() {
-	        return "login";
-	    }
-
-	    @RequestMapping(value="/logout")
-	    public String logout(HttpServletRequest request, HttpServletResponse response) {
-	        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-	        if (auth != null){
-	            new SecurityContextLogoutHandler().logout(request, response, auth);
-	        }
-	        request.setAttribute("logout","logout");
-	        return "login";
-	    }
-	    
-	}
-
+//package MovieDatabase.MovieDB;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class DefaultController {
+//	
+//	
+//	    @RequestMapping(value="/")
+//	    public String jspIndex() {
+//	        return "index";
+//	    }
+//
+//	    @RequestMapping(value="/login")
+//	    public String login() {
+//	        return "login";
+//	    }
+//
+//	    @RequestMapping(value="/logout")
+//	    public String logout(HttpServletRequest request, HttpServletResponse response) {
+//	        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//	        if (auth != null){
+//	            new SecurityContextLogoutHandler().logout(request, response, auth);
+//	        }
+//	        request.setAttribute("logout","logout");
+//	        return "login";
+//	    }
+//	    
+//	}
+//
